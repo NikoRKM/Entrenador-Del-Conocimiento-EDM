47144db93cfb7e65655294d8046df5eb34202192
barrier <facuro1000@outlook.com>
2022-09-02 04:16:56 -0300

    Main structure of the web

--- src/main/java/ar/edu/unju/edm/project/controllers/MainController.java
+++ src/main/java/ar/edu/unju/edm/project/controllers/MainController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    
+
     @GetMapping({ "/", "/index", "/home" })
-	public String getIndex() {
+    public String getIndex() {
         return "index";
+    }
+
+    @GetMapping("/recreationalActivities")
+    public String getRecreationalActivities() {
+
+        return "recreationalActivities";
     }
 }
