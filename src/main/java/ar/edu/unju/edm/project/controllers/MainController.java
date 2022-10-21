8f4686f620cedaee90a431f7a21533dc9398efc7
barrier <facuro1000@outlook.com>
2022-10-21 11:38:41 -0300

    Menu updated

--- src/main/java/ar/edu/unju/edm/project/controllers/MainController.java
+++ src/main/java/ar/edu/unju/edm/project/controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping({ "/", "/index", "/home" })
+    @GetMapping({ "/", "/index", "/home", "/menu" })
     public String getIndex() {
         return "index";
     }
